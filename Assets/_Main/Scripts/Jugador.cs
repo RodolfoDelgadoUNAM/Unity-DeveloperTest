5a6d6076299c5dadcff83b83378876bdd799c148
Rodolfo Delgado <rodo_ime@comunidad.unam.mx>
2021-10-23 02:24:06 -0500

    Animaciones jump, run, defeat, attack a jugador

--- Assets/_Main/Scripts/Jugador.cs
+++ Assets/_Main/Scripts/Jugador.cs
@@ -33,41 +33,55 @@ public class Jugador : MonoBehaviour
     [Tooltip("arrastra aqui el objeto camara")]
     public Transform PosicionCamara;
 
+    //Variables privadas
     CharacterController player;
-    float VelocidadRotacion;
-
+    Animator AnimacionJugador;
+    private float VelocidadRotacion;
     private Vector3 Salto;
+    [HideInInspector]
+    public bool JugadorAtacando;
+    [HideInInspector]
+    public bool JugadorCorriendo;
+    [HideInInspector]
+    public bool JugadorDefendiendo;
 
     #endregion
 
     void Start()
     {
         player = GetComponent<CharacterController>();
+        AnimacionJugador = GetComponent<Animator>();
     }
 
     void Update()
     {
         //Metodo control movimiento, parametros son los inputs del teclado
-        MovimientoJugador(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MovimientoJugador(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),JugadorAtacando);
 
-        //Metodo control gravedad
+        //Metodo Saltos
         SaltoJugador();
+
+        //Metodo Atacar condicion clic izquierdo presionado
+        AtaqueJugador(Input.GetMouseButton(0));
+
+        //Metodo Atacar condicion clic derechopresionado
+        DefensaJugador(Input.GetMouseButton(1));
 
     }
 
 
     #region Control Movimiento Jugador
 
-    public void MovimientoJugador(float horizontal, float vertical)
+    public void MovimientoJugador(float horizontal, float vertical, bool Atacando)
     {
         //se obtiene el vector de direccion de los controles
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-
-        //si se estan presionando los controles aplicar el movimiento
-        if (direction.magnitude > 0f)
+        //si se estan presionando los controles aplicar el movimiento y el jugador no esta atacando o defendiendo
+        if (direction.magnitude > 0f && !JugadorAtacando && !JugadorDefendiendo)
         {
-
+            //Se activan animacion corriendo
+            AnimacionCorrer(true);
 
             //Angulo de rotacion del jugador de acuerdo con los controles y la camara
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + PosicionCamara.eulerAngles.y;
@@ -83,27 +97,92 @@ public class Jugador : MonoBehaviour
 
             //mueve al jugador de acuerdo con la direccion y la velocidad
             player.Move(moveDir.normalized * VelocidadJugador * Time.deltaTime);
+
+            //Se utiliza esta variable para evitar que el jugador ataque mientras corre
+            JugadorCorriendo = true;
         }
+        else
+        {
+            //Se desactiva animacion corriendo
+            AnimacionCorrer(false);
+            JugadorCorriendo = false;
+        }
+
     }
 
     public void SaltoJugador()
     {
-        if (player.isGrounded && Salto.y < 0)
-        {
-            Salto.y = 0f;
-        }
-        if (Input.GetButtonDown("Jump") && player.isGrounded)
-        {
-            Salto.y += Mathf.Sqrt(PoderSalto * -3.0f * Gravedad);
-        }
 
+        if (player.isGrounded && Salto.y<0)
+        {
+            Salto.y = 0;
+            AnimacionSalto(false);
+        }
+        //Si es presiona boton de salto y el jugador esta en el suelo y no esta atacando
+        if (Input.GetButtonDown("Jump") && player.isGrounded && !JugadorAtacando)
+        {
+            Salto.y += Mathf.Sqrt(PoderSalto *-2 *Gravedad);
+            AnimacionSalto(true);
+        }
+        //vector de salto es igual a la gravedad
         Salto.y += Gravedad * Time.deltaTime;
         player.Move(Salto * Time.deltaTime);
     }
 
+    public void AtaqueJugador(bool ControlAtaque)
+    {
+        if(ControlAtaque && !JugadorCorriendo && !JugadorDefendiendo)
+        {
+            JugadorAtacando = true;
+        }
+        else
+        {
+            JugadorAtacando = false;
+        }
+
+        AnimacionAtacar(JugadorAtacando);
+
+    }
+
+    public void DefensaJugador(bool ControlDefensa)
+    {
+        if (ControlDefensa && !JugadorCorriendo &&!JugadorAtacando)
+        {
+            JugadorDefendiendo = true;
+        }
+        else
+        {
+            JugadorDefendiendo = false;
+        }
+
+        AnimacionDefender(JugadorDefendiendo);
+
+    }
+
     #endregion
 
+    #region Animaciones
+    public void AnimacionSalto(bool Saltando)
+    {
+        AnimacionJugador.SetBool("Salto", Saltando);
+    }
 
+    public void AnimacionCorrer(bool Corriendo)
+    {
+        AnimacionJugador.SetBool("Correr", Corriendo);
+    }
+
+    public void AnimacionAtacar(bool Atacando)
+    {
+        AnimacionJugador.SetBool("Atacar", Atacando);
+    }
+
+    public void AnimacionDefender(bool Defendiendo)
+    {
+        AnimacionJugador.SetBool("Defender", Defendiendo);
+    }
+
+    #endregion
 }
 
 
