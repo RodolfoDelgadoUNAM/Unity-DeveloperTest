654c5f9594e930bdd2e88bb6a06a5ccb0ecfec61
Rodolfo Delgado <rodo_ime@comunidad.unam.mx>
2021-10-24 01:21:27 -0500

    e anaden efectos a escenario, Cielo se cambia por noche, se agrega musica de caballeros, se importa ocean y se configura.

---
+++ Assets/External Assets/crest-master/crest-master/crest/Assets/Crest/Crest/Scripts/Helpers/Helpers.cs
@@ -0,0 +1,26 @@
+// Crest Ocean System
+
+// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
+
+namespace Crest
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// General purpose helpers which, at the moment, do not warrant a seperate file.
+    /// </summary>
+    public static class Helpers
+    {
+        public static bool IsMSAAEnabled(Camera camera)
+        {
+            return camera.allowMSAA && QualitySettings.antiAliasing > 0f;
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+}
